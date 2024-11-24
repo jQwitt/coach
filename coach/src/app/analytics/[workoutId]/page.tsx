fce84cc64811e1c1d5143badc601b106cfa30507
Jack Witt <Jwitt1452@gmail.com>
2024-11-24 12:44:39 -0800

    fix: add PageProps

--- coach/src/app/analytics/[workoutId]/page.tsx
+++ coach/src/app/analytics/[workoutId]/page.tsx
@@ -1,8 +1,10 @@
+import { PageProps } from ".next/types/app/page";
+
 export default async function WorkoutAnalyticsPage({
   params,
 }: {
   params: { workoutId: string };
-}) {
+} & PageProps) {
   const { workoutId } = params;
 
   return <div>workout by id {workoutId}</div>;
