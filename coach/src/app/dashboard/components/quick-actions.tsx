c885ba576d7084e602bdb8cb0eb6b9d2ded3d2e0
Jack Witt <Jwitt1452@gmail.com>
2024-11-24 19:15:11 -0800

    feat: add init cycling

--- coach/src/app/dashboard/components/quick-actions.tsx
+++ coach/src/app/dashboard/components/quick-actions.tsx
@@ -8,12 +8,20 @@ export default function QuickActions() {
   return (
     <div className="space-y-2">
       <Header title="Jump Back In" level={HeaderLevel.SECTION} />
-      <Button
-        className="w-full sm:w-fit"
-        onClick={() => redirect("/log-workout/lifting")}
-      >
-        Log a Workout
-      </Button>
+      <div className="flex flex-col sm:flex-row gap-2">
+        <Button
+          className="w-full sm:w-fit"
+          onClick={() => redirect("/log-workout/lifting")}
+        >
+          Log Weights
+        </Button>
+        <Button
+          className="w-full sm:w-fit"
+          onClick={() => redirect("/log-workout/cycling")}
+        >
+          Log Cycling
+        </Button>
+      </div>
     </div>
   );
 }
--- coach/src/app/log-workout/cycling/page.tsx
+++ coach/src/app/log-workout/cycling/page.tsx
@@ -1,12 +1,11 @@
 import Image from "next/image";
 import Barbell from "../../../../public/images/dumbbell_black.png";
 
+import { Bike } from "lucide-react";
+
 import WorkoutForm from "@/components/forms/workouts/workout-form";
 import { getCurrentUser } from "@/app/actions";
 import Header from "@/components/ui/header";
-
-import WorkoutTimeline from "./components/workout-timeline";
-import WorkoutStats from "./components/workout-stats";
 
 export default async function LogWorkoutLifting() {
   const { id } = (await getCurrentUser()) ?? {};
@@ -18,20 +17,10 @@ export default async function LogWorkoutLifting() {
   return (
     <div className="text-primary mx-auto p-4 max-w-4xl">
       <div className="flex items-center">
-        <Image
-          src={Barbell}
-          alt="dumbbell logo"
-          width={48}
-          height={48}
-          className="rotate-45 -mt-1"
-        />
-        <Header title="Weights" />
+        <Bike className="-mt-1 font-bold" size={48} />
+        <Header title="Cycling" />
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-        <WorkoutForm userId={id} />
-        <WorkoutTimeline />
-        <WorkoutStats />
-      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6">Coming soon!</div>
     </div>
   );
 }
