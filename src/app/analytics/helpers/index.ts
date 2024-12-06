a916518380696106f32a7bd3e14dbf4ff66e7bf7
Jack Witt <Jwitt1452@gmail.com>
2024-12-05 17:18:59 -0800

    feat: add getDateRangeISOs

--- src/app/analytics/helpers/index.ts
+++ src/app/analytics/helpers/index.ts
@@ -20,3 +20,21 @@ export const getWorkoutStatistics = (workouts: WorkoutLiftingData[]) => {
 
 	return result;
 };
+
+export function getDateRangeISOs() {
+	const now = new Date();
+	const week = new Date(now);
+	week.setDate(week.getDate() - 7);
+	const month = new Date(now);
+	month.setMonth(month.getMonth() - 1);
+	const year = new Date(now);
+	year.setFullYear(year.getFullYear() - 1);
+
+	return {
+		day: now.toISOString(),
+		week: week.toISOString(),
+		month: month.toISOString(),
+		year: year.toISOString(),
+		"all-time": new Date(0).toISOString(),
+	};
+}
