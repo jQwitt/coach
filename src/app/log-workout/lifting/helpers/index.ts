a31bb151ffe4386f4bf42c8a813a42e34cad8bea
Jack Witt <Jwitt1452@gmail.com>
2024-12-11 14:28:29 -0800

    fix: jokes, add back in definitely used

---
+++ src/app/log-workout/lifting/helpers/index.ts
@@ -0,0 +1,18 @@
+import type { WorkoutLiftingData } from "@/lib/types";
+export const getRepsPerExercise = (
+	workout: Omit<WorkoutLiftingData, "date">,
+): Array<{ index: number; reps: number }> => {
+	return workout.exercises.map((exercise, index) => {
+		const { sets } = exercise;
+		const reps = sets.reduce((acc, { count, reps }) => acc + count * reps, 0);
+		return { index, reps };
+	});
+};
+export const getSetsPerExercise = (
+	workout: Omit<WorkoutLiftingData, "date">,
+): Array<{ index: number; sets: number }> => {
+	return workout.exercises.map(({ sets }, index) => ({
+		index,
+		sets: sets.reduce((acc, { count }) => acc + count, 0),
+	}));
+};
