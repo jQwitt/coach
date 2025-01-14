3668ac7fa7190d52ec734ef4fa06138abd087a27
Jack Witt <Jwitt1452@gmail.com>
2025-01-14 13:08:48 -0800

    feat: add entry point

--- src/app/dashboard/components/quick-actions.tsx
+++ src/app/dashboard/components/quick-actions.tsx
@@ -10,7 +10,18 @@ export default function QuickActions() {
 		<div className="space-y-2">
 			<Header title="Jump Back In" level={HeaderLevel.SECTION} />
 			<div className="flex flex-col gap-2 sm:flex-row">
-				<Button className="w-full sm:w-fit group" onClick={() => redirect("/log-workout")}>
+				<Button className="w-full sm:w-fit group" onClick={() => redirect("/live-coach")}>
+					Chat with Live Coach
+					<ArrowRight
+						size={16}
+						className="group-hover:translate-x-1 transition-all ease-in duration-100"
+					/>
+				</Button>
+				<Button
+					className="w-full sm:w-fit group"
+					onClick={() => redirect("/log-workout")}
+					variant="secondary"
+				>
 					Log a Workout
 					<ArrowRight
 						size={16}
